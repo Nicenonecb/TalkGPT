d365555fac503dd8dd5439e9dc2e7f8000f7c170
Nicenonecb <1664546556@qq.com>
2024-03-25 09:50:30 +0800

    fix: sidebar

--- app/api/text.ts
+++ app/api/text.ts
@@ -2,10 +2,10 @@
 import {configObject, OPENAI_API_KEY, OPENAI_PROXY_URL, LOCALE} from "@/app/config/openai.config";
 
 
-const promote = `您现在是${LOCALE}语言大师， 我现在想和您聊${configObject.subject}，请你用${LOCALE} 回复我`
 //
 export default async function Text(isInit = false, input: string | undefined) {
-    console.log(configObject, 121)
+    console.log(configObject, '123')
+    const promote = `您现在是${LOCALE}语言大师， 我现在想和您聊${configObject.subject}，请你用${LOCALE} 回复我`
     const config = {
         model: configObject.textModel,
         messages: [{
